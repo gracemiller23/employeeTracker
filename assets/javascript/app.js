0c0501a6f2fabad946f725ba237250d9cf96601b
Brian Witte <brianw0991@gmail.com>
2018-05-02 10:26:34 -0500

    added JS to send input values to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,5 @@
 
-var employeeName;
-var role;
-var startDate;
-var time;
-var rate;
-var total;
+
 
   // Initialize Firebase
   var config = {
@@ -18,3 +13,31 @@ var total;
   firebase.initializeApp(config);
 
 var database = firebase.database();
+
+var employeeName = "";
+var role = "";
+var startDate = "";
+var time = "";
+var rate = "";
+var total = "";
+
+$("#").on("click", function(event){
+
+    event.preventDefault();
+
+    employeeName = $("#ename").val().trim();
+    role = $("#role").val().trim();
+    startDate = $("#sdate").val().trim();
+    // time = $("#").val().trim();
+    rate = $("#mrate").val().trim();
+
+    database.ref().push({
+        employeeName: employeeName,
+        role: role,
+        startDate: startDate,
+        time: time,
+        rate: rate,
+        dateAdded:
+        firebase.database.ServerValue.TIMESTAMP
+    });
+});
